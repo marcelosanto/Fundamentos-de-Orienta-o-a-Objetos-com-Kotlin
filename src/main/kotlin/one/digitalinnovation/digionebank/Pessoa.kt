997e59713e4a2d56bfbc4ece4c4d62411951f13a
Marcelo Santos <rocha.mer21@gmail.com>
2021-05-25 00:35:46 -0300

    criada class conta, com as funções deposito e saque

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,11 +3,19 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome: String = "Marcelo"
     var cpf: String = "112.258.669-98"
+
+    private  set
+
+    constructor()
+
+    fun uneNomeCpf() = "${nome} e ${cpf}"
 }
 
 fun main() {
     val pessoa = Pessoa()
 
+    println(pessoa)
     println(pessoa.nome)
     println(pessoa.cpf)
+    println(pessoa.uneNomeCpf())
 }
